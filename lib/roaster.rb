ed354d54e11f35d81bebe29020d8ab96e3024a97
Nicolas Albeza <n.albeza@gmail.com>
2014-09-09 16:14:34 +0200

    require

--- lib/roaster.rb
+++ lib/roaster.rb
@@ -1,4 +1,9 @@
+require 'roaster/adapters/active_record'
 require 'roaster/decorator'
+require 'roaster/jsonapi'
+require 'roaster/query'
+require 'roaster/request'
+require 'roaster/version'
 
 module Roaster
 end
