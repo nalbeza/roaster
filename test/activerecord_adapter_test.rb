6c5c54a2bf2ac1ac8ad34efa6310953aed318db1
Nicolas Albeza <n.albeza@gmail.com>
2014-09-10 15:19:14 +0200

    refactoring

--- test/activerecord_adapter_test.rb
+++ test/activerecord_adapter_test.rb
@@ -8,38 +8,35 @@ require_relative 'test_helper'
 
 class ActiveRecordAdapterTest < MiniTest::Unit::TestCase
 
-  private
-  def call_adapter_method(method)
-    target = Roaster::Query::Target.new
-    mapping = AlbumMapping
-    params = {
-    }
-    query = Roaster::Query.new(target, mapping, params)
-    ar_query = @album_adapter.send(method, query)
-  end
-
-  public
-
   def setup
     super
     @album_adapter = Roaster::Adapters::ActiveRecord.new(Album)
+    @mapping = AlbumMapping
   end
 
   def test_create
-    call_adapter_method :create
+    q = call_adapter_method :create
   end
 
   def test_read
-    call_adapter_method :read
+    q = call_adapter_method :read
   end
 
   def test_update
-    call_adapter_method :update
+    q = call_adapter_method :update
   end
 
-
   def test_delete
-    call_adapter_method :delete
+    q = call_adapter_method :delete
+  end
+
+  private
+
+  def call_adapter_method(method,
+                          target = Roaster::Query::Target.new,
+                          params = {})
+    query = Roaster::Query.new(target, @mapping, params)
+    @album_adapter.send(method, query)
   end
 
 end
