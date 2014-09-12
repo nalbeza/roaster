9acd85aa2ffbdc26e236917b0b771f0b78fc871b
Nicolas Albeza <n.albeza@gmail.com>
2014-09-12 18:22:56 +0200

    moar tests

--- lib/roaster/resource.rb
+++ lib/roaster/resource.rb
@@ -4,8 +4,19 @@ module Roaster
     
     attr_reader :target
 
-    def initialize(target, opts = {})
+    def initialize(target, adapter_class, opts = {})
       @target = target
+      model_class = opts[:model_class] || self.class.model_class_from_target(target)
+      @adapter = adapter_class.new(model_class)
+    end
+
+    def query(query)
+      @adapter.read(query)
+    end
+
+    #TODO: Move this elsewhere (factory)
+    def self.model_class_from_target(target)
+      "#{target.resource_name.to_s.singularize}".classify.constantize
     end
 
   end
