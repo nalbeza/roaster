674c433b757d967cd80ea6f99b87f4cac7ff7d4a
Nicolas Albeza <n.albeza@gmail.com>
2014-11-14 16:32:22 +0100

    bugfix

--- lib/roaster/factory.rb
+++ lib/roaster/factory.rb
@@ -6,7 +6,7 @@ module Roaster
         rel_name = target.relationship_name
         if rel_name
           all_rels = resource_mapping.representable_attrs.values_at(:_has_many, :_has_one).flatten.compact
-          r = all_rels.find {|r| r[:as].to_sym == name.to_sym }
+          r = all_rels.find {|r| r[:as].to_sym == rel_name.to_sym }
           rel_name = r[:mapping] if r && r[:mapping]
           mapping_class_from_name(rel_name)
         else
