5a17a2fea776d200dae8d817c485c45de77e9ce0
Nicolas Albeza <n.albeza@gmail.com>
2014-11-14 16:10:23 +0100

    Fixed rel mapping parameter when fetching links

--- lib/roaster/factory.rb
+++ lib/roaster/factory.rb
@@ -2,10 +2,14 @@ module Roaster
   class Factory
     class << self
       def mapping_class_from_target(target)
+        resource_mapping = mapping_class_from_name(target.resource_name)
         if target.relationship_name
-          mapping_class_from_name(target.relationship_name)
+          all_rels = resource_mapping.representable_attrs.values_at(:_has_many, :_has_one).flatten
+          r = all_rels.find {|r| r[:as].to_sym == name.to_sym }
+          rel_name = r && r[:mapping] ? r[:mapping] : target.relationship_name
+          mapping_class_from_name(rel_name)
         else
-          mapping_class_from_name(target.resource_name)
+          resource_mapping
         end
       end
 
